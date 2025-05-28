021d9f27d4489ea63f4ce810b8a084c18666fefb
Dhanashree-Patil95 <dhanashree.patil@tekditechnologies.com>
2025-05-28 17:28:28 +0530

    feat:Created API for  demnad

--- controller/cost_controller.go
+++ controller/cost_controller.go
@@ -4,20 +4,45 @@ import (
 	"buildingcost/service"
 	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func CalculateCost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	code := vars["code"]
+
 	var request map[string]interface{}
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		http.Error(w, "Invalid request", http.StatusBadRequest)
 		return
 	}
 
-	if err := service.ProcessApplications(request); err != nil {
+	if err := service.ProcessApplications(code, request); err != nil {
 		http.Error(w, "Processing error: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(request)
+}
+
+func GeneratetDemands(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	code := vars["code"]
+
+	var requestBody map[string]interface{}
+	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	responseBody, err := service.CreateDemands(code, requestBody)
+	if err != nil {
+		http.Error(w, "Error fetching demands: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(responseBody)
 }
